3361d8f7aa164628e99a60740537cdc5d312b396
H4R02 <sanjanag.plakhisarai@gmail.com>
2025-08-26 08:18:10 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # basic bro
 import os
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-SESSION_STRING = os.getenv("SESSION_STRING", "")
-UNPLASH_API = os.getenv("UNPLASH_API", "")
+API_ID = int(os.getenv("API_ID", "26939277"))
+API_HASH = os.getenv("API_HASH", "4c5efa0dd5a7a816bd4becdbbeadb6de")
+SESSION_STRING = os.getenv("SESSION_STRING", "1AZWarzsBuxMeMOd1SVw-d5o0mSq-FLmMt8fmnJ3L_2lOfrawikCpa5A2Adbikeu-hVQ60Y1y_aCd0nN8mlknyHDG5jMZqnsH9vLxyMqat7K65J7pMHlV0viyQVfTccMnlACv-EkCzv6xFdiaUXscpbv_BANx7-LVJieDgsXkXRdTgSXhuZ-RtxTP8EBqGW-yGTNT1kBlHFrVF5KJ-rkE-F8mZ-XheFKC1IZNNDafNjHnFEe0k0GALcahTFcPBcvERnD2tRQRhrt9bUc_TuJ_CrMc_K1BtTt_dT-30DrV_MGKO-jno6iYmQJq-JfL-MpovcwsI37n6UQniEG1JcHstm24ncPMNKg=")
+UNPLASH_API = os.getenv("UNPLASH_API", "E5_hCU88oexAiuRVUyDWBTO2IQoAhwmecWZPL4yNBwc")
